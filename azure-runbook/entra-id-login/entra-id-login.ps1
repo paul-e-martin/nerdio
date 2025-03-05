1e37ef919f76d148d44c88778743ab74b6d58b62
paul-e-martin <34447549+paul-e-martin@users.noreply.github.com>
2025-03-05 12:06:21 +0000

    refactor: Simplify variable initialization and context setup in Entra ID and Integrated Monitoring scripts (#10)

--- azure-runbook/entra-id-login/entra-id-login.ps1
+++ azure-runbook/entra-id-login/entra-id-login.ps1
@@ -10,30 +10,26 @@
         This Script will install the Microsoft AADLogin extension on the Azure VM.
 #>
 
-begin {
-    # Ensure context is using correct subscription
-    Set-AzContext -SubscriptionId $AzureSubscriptionId
+# Ensure context is using correct subscription
+Set-AzContext -SubscriptionId $AzureSubscriptionId
 
-    # Variables
-    $AzVM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
-    $PublisherName = "Microsoft.Azure.ActiveDirectory"
-    $Type = "AADLoginForWindows"
-    $name = 'AADLogin'
+# Variables
+$AzVM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
+$PublisherName = "Microsoft.Azure.ActiveDirectory"
+$Type = "AADLoginForWindows"
+$name = 'AADLogin'
 
-    # Get the latest major version
-    $version = ((Get-AzVMExtensionImage -Location $AzVM.Location -PublisherName $PublisherName -Type $Type).Version[-1][0..2] -join '')
+# Get the latest major version
+$version = ((Get-AzVMExtensionImage -Location $AzVM.Location -PublisherName $PublisherName -Type $Type).Version[-1][0..2] -join '')
+
+#enable the Microsoft Active Directory Login Extension
+$AADExtension = @{
+    ResourceGroupName  = $AzVM.ResourceGroupName
+    Location           = $AzVM.Location
+    VMName             = $AzureVMName
+    Name               = $name
+    Publisher          = $PublisherName
+    ExtensionType      = $Type
+    TypeHandlerVersion = $version
 }
-
-process {
-    #enable the Microsoft Active Directory Login Extension
-    $AADExtension = @{
-        ResourceGroupName  = $AzVM.ResourceGroupName
-        Location           = $AzVM.Location
-        VMName             = $AzureVMName
-        Name               = $name
-        Publisher          = $PublisherName
-        ExtensionType      = $Type
-        TypeHandlerVersion = $version
-    }
-    Set-AzVMExtension @AADExtension
-}
+Set-AzVMExtension @AADExtension
