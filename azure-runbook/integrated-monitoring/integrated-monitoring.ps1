12992f6f64f237b231d30597ec461a2da3a5599b
Paul Martin <paul.martin@wavenet.co.uk>
2025-03-05 12:06:29 +0000

    refactor: Simplify variable initialization and context setup in Entra ID and Integrated Monitoring scripts

--- azure-runbook/integrated-monitoring/integrated-monitoring.ps1
+++ azure-runbook/integrated-monitoring/integrated-monitoring.ps1
@@ -10,34 +10,30 @@
         This Script will install the Microsoft GuestAttestation extension on the Azure VM.
 #>
 
-begin {
-    # Set Error action
-    $errorActionPreference = "Stop"
+# Set Error action
+$errorActionPreference = "Stop"
 
-    # Ensure context is using correct subscription
-    Set-AzContext -SubscriptionId $AzureSubscriptionId
+# Ensure context is using correct subscription
+Set-AzContext -SubscriptionId $AzureSubscriptionId
 
-    # Variables
-    $AzVM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
-    $PublisherName = "Microsoft.Azure.Security.WindowsAttestation"
-    $name = 'GuestAttestation'
-    $Type = "GuestAttestation"
+# Variables
+$AzVM = Get-AzVM -Name $AzureVMName -ResourceGroupName $AzureResourceGroupName
+$PublisherName = "Microsoft.Azure.Security.WindowsAttestation"
+$name = 'GuestAttestation'
+$Type = "GuestAttestation"
 
-    # Get the latest major version
-    $version = ((Get-AzVMExtensionImage -Location $AzVM.Location -PublisherName $PublisherName -Type $Type).Version[-1][0..2] -join '')
+# Get the latest major version
+$version = ((Get-AzVMExtensionImage -Location $AzVM.Location -PublisherName $PublisherName -Type $Type).Version[-1][0..2] -join '')
+
+#enable the Microsoft Guest Attestation Extension
+$AADExtension = @{
+    ResourceGroupName      = $AzVM.ResourceGroupName
+    Location               = $AzVM.Location
+    VMName                 = $AzureVMName
+    Name                   = $name
+    Publisher              = $PublisherName
+    ExtensionType          = $Type
+    TypeHandlerVersion     = $version
+    EnableAutomaticUpgrade = $true
 }
-
-process {
-    #enable the Microsoft Guest Attestation Extension
-    $AADExtension = @{
-        ResourceGroupName      = $AzVM.ResourceGroupName
-        Location               = $AzVM.Location
-        VMName                 = $AzureVMName
-        Name                   = $name
-        Publisher              = $PublisherName
-        ExtensionType          = $Type
-        TypeHandlerVersion     = $version
-        EnableAutomaticUpgrade = $true
-    }
-    Set-AzVMExtension @AADExtension
-}
+Set-AzVMExtension @AADExtension
